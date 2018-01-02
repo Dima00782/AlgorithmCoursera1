f629f0d64d65182ec58e9db4778328c0871b8bfd
Dima00782 <dima00782@gmail.com>
2018-01-02 23:53:19 +0700

    add first double linked list version

--- week2/Deque.java
+++ week2/Deque.java
@@ -1,10 +1,25 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.lang.IllegalArgumentException;
+import java.lang.UnsupportedOperationException;
 
 public class Deque<Item> implements Iterable<Item> {
+    private Node first = null;
+    private Node last = null;
+
+    private int numberOfElements;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node previous;
+    }
 
     private class DequeIterator implements Iterator<Item> {
-        public boolean hasNext() {
+        private Node current = last;
 
+        public boolean hasNext() {
+            return current != null;
         }
 
         public void remove() {
@@ -12,44 +27,79 @@ public class Deque<Item> implements Iterable<Item> {
         }
 
         public Item next() {
-
+            Item item = current.item;
+            current = current.previous;
+            return item;
         }
 
     }
 
     // construct an empty deque
     public Deque() {
-
+        numberOfElements = 0;
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-
+        return numberOfElements == 0;
     }
 
     // return the number of items on the deque
     public int size() {
-
+        return numberOfElements;
     }
 
     // add the item to the front
     public void addFirst(Item item) {
-
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        first.previous = null;
+        if (isEmpty()) {
+            last = first;    
+        } else {
+            oldFirst.previous = first;
+        }
+        ++numberOfElements;
     }
 
     // add the item to the end
     public void addLast(Item item) {
-
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        last.previous = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            last.previous = oldLast;
+            oldLast.next = last;
+        }
+        ++numberOfElements;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item item = first.item;
+        first = first.next;
+        --numberOfElements;
+        return item;
     }
 
     // remove and return the item from the end
     public Item removeLast() {
-
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item item = last.item;
+        last = last.previous;
+        --numberOfElements;
+        return item;
     }
 
     // return an iterator over items in order from front to end
@@ -59,6 +109,15 @@ public class Deque<Item> implements Iterable<Item> {
 
     // unit testing (optional)
     public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        deque.addFirst(5);
 
+        for (int val : deque) {
+            System.out.println(val);
+        }
     }
 }
