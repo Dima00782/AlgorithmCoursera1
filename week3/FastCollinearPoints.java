0dd68d4ac8c041640bf6ff3185a923ef4d95fcad
Dima00782 <dima00782@gmail.com>
2018-01-13 14:44:52 +0700

    fix duplicate in intervals

--- week3/FastCollinearPoints.java
+++ week3/FastCollinearPoints.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class FastCollinearPoints {
     private static final int MIN_NUMBER_OF_POINTS = 4;
-    private LineSegment[] lineSegments;
+    private final ArrayList<LineSegment> segments;
 
     private void checkNoRepeated(Point[] points) {
         for (int i = 1; i < points.length; ++i) {
-            if (points[i - 1] == null || points[i] == null || points[i - 1].compareTo(points[i]) == 0) {
+            if (points[i - 1].compareTo(points[i]) == 0) {
                 throw new IllegalArgumentException();
             }
         }
     }
 
-    private void addSegmentToResult(ArrayList<LineSegment> result, Point[] points, int begin, int end, Point origin, boolean[] used) {
+    private void checkNoNull(Point[] points) {
+        for (Point p : points) {
+            if (p == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void addSegmentToResult(ArrayList<LineSegment> result, Point[] points, int begin, int end, Point origin) {
         Point max = origin;
         Point min = origin;
         for (int i = begin; i != end; ++i) {
@@ -23,10 +32,11 @@ public class FastCollinearPoints {
             if (points[i].compareTo(max) > 0) {
                 max = points[i];
             }
-            used[i] = true;
         }
 
-        result.add(new LineSegment(min, max));
+        if (min.compareTo(origin) == 0) {
+            result.add(new LineSegment(min, max));
+        }
     }
 
     // finds all line segments containing 4 or more points
@@ -34,44 +44,40 @@ public class FastCollinearPoints {
         if (points == null) {
             throw new IllegalArgumentException();
         }
-        Arrays.sort(points);
-        checkNoRepeated(points);
+        checkNoNull(points);
 
-        boolean[] used = new boolean[points.length];
-        ArrayList<LineSegment> segments = new ArrayList<LineSegment>();
-        int size = points.length;
-        for (int i = 0; i + (MIN_NUMBER_OF_POINTS - 1) < points.length; ++i) {
+        Point[] copiedPoints = new Point[points.length];
+        System.arraycopy(points, 0, copiedPoints, 0, points.length);
+        Arrays.sort(copiedPoints);
+        checkNoRepeated(copiedPoints);
+
+        segments = new ArrayList<LineSegment>();
+        for (int i = 0; i < points.length; ++i) {
             Point origin = points[i];
-            if (used[i]) {
-                continue;
-            }
-            Arrays.sort(points, i + 1, points.length, origin.slopeOrder());
+            Arrays.sort(copiedPoints, origin.slopeOrder());
 
-            // skip first because it is a origin because a.slopeTo(a) == -inf
-            int first = i + 1;
+            int first = 1;
             int second = first + 1;
-            while (first < points.length && second < points.length) {
-                double firstSlope = origin.slopeTo(points[first]);
-                while (second < points.length && origin.slopeTo(points[second]) == firstSlope) ++second;
+            while (first < copiedPoints.length && second < copiedPoints.length) {
+                double firstSlope = origin.slopeTo(copiedPoints[first]);
+                while (second < copiedPoints.length && origin.slopeTo(copiedPoints[second]) == firstSlope) ++second;
                 // plus 1 for origin
                 if (second - first + 1 >= MIN_NUMBER_OF_POINTS) {
-                    addSegmentToResult(segments, points, first, second, origin, used);
+                    addSegmentToResult(segments, copiedPoints, first, second, origin);
                 }
                 first = second;
                 second = first + 1;
             }
         }
-
-        lineSegments = segments.toArray(new LineSegment[segments.size()]);
     }
 
     // the number of line segments
     public int numberOfSegments() {
-    	return lineSegments.length;
+    	return segments.size();
     }
 
     // the line segments
     public LineSegment[] segments() {
-    	return lineSegments;
+        return segments.toArray(new LineSegment[segments.size()]);
     }
 }
