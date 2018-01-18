1a1fdaf98b2fbe535392d455a9d391f9488c5617
Dima00782 <dima00782@gmail.com>
2018-01-18 20:26:19 +0700

    impl hamming

--- week4/Board.java
+++ week4/Board.java
@@ -1,8 +1,19 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
 public class Board {
+    private final int[][] field;
+    private int numberOfMoves;
 
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
+        field = new int[blocks.length][];
+        for (int i = 0; i < field.length; ++i) {
+            field[i] = Arrays.copyOf(blocks[i], blocks[i].length);
+        }
     }
 
     // board dimension n
@@ -12,7 +23,17 @@ public class Board {
 
     // number of blocks out of place
     public int hamming() {
-        return 0;
+        int distance = 0;
+        for (int i = 0; i < field.length; ++i) {
+            for (int j = 0; j < field[i].length; ++j) {
+                int goal = i * field[i].length + j + 1;
+                if (goal == field.length * field.length) {
+                    goal = 0;
+                }
+                distance += goal != field[i][j] ? 1 : 0;
+            }
+        }
+        return numberOfMoves + distance;
     }
 
     // sum of Manhattan distances between blocks and goal
@@ -47,5 +68,16 @@ public class Board {
 
     // unit tests (not graded)
     public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                blocks[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(blocks);
+        StdOut.println(initial.hamming());
     }
 }
